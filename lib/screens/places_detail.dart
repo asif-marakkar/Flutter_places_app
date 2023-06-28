a566bb36794521ab28169110c75a26b1070aeda4
Muhammed Asif <machoasif@zoho.com>
2023-06-28 15:40:03 +0530

    location package

--- lib/screens/places_detail.dart
+++ lib/screens/places_detail.dart
@@ -8,8 +8,8 @@ class PlacesDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //final textTheme = Theme.of(context).textTheme;
-    //final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
       appBar: AppBar(
         title: Text(place.title),
@@ -22,6 +22,26 @@ class PlacesDetailScreen extends StatelessWidget {
             width: double.infinity,
             height: double.infinity,
           ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                const CircleAvatar(),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    place.location.address,
+                    textAlign: TextAlign.center,
+                    style: textTheme.titleLarge!.copyWith(
+                      color: colorScheme.onBackground,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
